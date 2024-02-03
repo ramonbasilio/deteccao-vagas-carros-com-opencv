5eda39089d122fb29629fa41aa7b064b7dec9f14
ramon <ramon.basilio@hotmail.com>
2024-02-03 00:16:19 -0300

    Commit final

--- contadorVagas.py
+++ contadorVagas.py
@@ -15,12 +15,23 @@ while True:
     imgThold = cv2.adaptiveThreshold(imgCinza, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 25, 16)
     imgMedian = cv2.medianBlur(imgThold, 5)
     imgDilat = cv2.dilate(imgMedian, kernel)
-
+    vagasAbertas = 0
     for x, y, w, h in vagas:
+        vaga = imgDilat[y:y+h, x:x+w]
+        count = cv2.countNonZero(vaga)
+        cv2.putText(img, str(count), (x, y+h-10),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1)
         cv2.rectangle(img, (x,y),(x+w, y+h), (255,0,0),2)
+        if count < 900:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            vagasAbertas += 1
+        else:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+        cv2.rectangle(img, (90,0),(450,60), (0,255,0),-1)
+        cv2.putText(img, f'Livre: {vagasAbertas} / 69', (95,45), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,255,255), 5)
+
+    print(vagasAbertas)
     cv2.imshow('Video', img)
-    cv2.imshow('Video Thold', imgThold)
-    cv2.imshow('Video Dilat', imgDilat)
-
-
+    # cv2.imshow('Video Thold', imgThold)
+    # cv2.imshow('Video Dilat', imgDilat)
     cv2.waitKey(10)
